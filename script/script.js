946507400c3e4783a5e48086e23e318cee798f7c
Jenya Sergeev <multiko@narod.ru>
2022-09-28 12:48:53 +0300

    fixed

--- script/script.js
+++ script/script.js
@@ -1,18 +1,25 @@
-const nameProduct = prompt('Наименование товара :');
-alert ("Выбрано: " + nameProduct);
+{
+const nameProduct = "Пальто";
+const quantityGoods = "Количество товара";
+const categoryGoods = "Категория товара";
+const priceGoods = "Цена товара";
+
+console.log (nameProduct);
+}
+
+{
+const nameProductGet = prompt('Наименование товара :');
 const quantityGoods = prompt("Выберите количество товара: ");
 let quantityGoods_num = Number(quantityGoods);
-alert ("Количество товара: " + quantityGoods);
 const categoryGoods = prompt('Категория товара');
-alert ("Категория товара: " + categoryGoods);
 const priceGoods = prompt('Цена товара');
 let priceGoods_num = Number(priceGoods);
-alert ("Ваша цена: " + priceGoods);
 
-console.log (typeof nameProduct)
+console.log (typeof nameProductGet)
 console.log (typeof quantityGoods_num)
 console.log (typeof categoryGoods)
 console.log (typeof priceGoods_num)
 
-console.log("На складе " + nameProduct + " в количестве: " + quantityGoods_num + " из категории: " + categoryGoods + " на сумму: " + priceGoods * quantityGoods_num)
+console.log("На складе " + nameProductGet + " в количестве: " + quantityGoods_num + " из категории: " + categoryGoods + " на сумму: " + priceGoods * quantityGoods_num)
+}
 
