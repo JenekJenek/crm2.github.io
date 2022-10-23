4a4cf6aa991baf01ddeb53442b06ee048794caf4
Jenya Sergeev <multiko@narod.ru>
2022-10-23 15:54:18 +0300

    fixed

--- script/lesson08/game.js
+++ script/lesson08/game.js
@@ -1,13 +1,13 @@
 let input, number, random = Math.floor(Math.random() * 100);
 
 while ((input = prompt("Загадано число от 0 до 100. Попробуйте угадать!")) !== null) {
-    if ((number = +input) !== parseFloat(input) || !Number.isFinite(number))
-        alert("Введите число!");
-    else if (number > random)
+    if ((number = +input) !== parseFloat(input) || !Number.isFinite(number)) {
+        alert("Введите число!"); 
+    } else if (number > random) {
         alert("Меньше!");
-    else if (number < random)
+    } else if (number < random) {
         alert("Больше!");
-    else if (number == random) {
+    } else if (number == random) {
         alert("Правильно!");
         break;
     }
